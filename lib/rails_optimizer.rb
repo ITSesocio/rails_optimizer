230f771ef74e133e6a75212346bdb8542272a26d
Facundo A. Díaz Martínez <facundo.martinez@sesocio.com>
2021-01-08 13:03:12 -0300

    Change version

--- lib/rails_optimizer.rb
+++ lib/rails_optimizer.rb
@@ -30,12 +30,16 @@ module RailsOptimizer
 		def self.belongs_to(name, scope = nil, **options)
 			super
 			define_method name.to_s do |*args|
-				super if args.empty?
-				if options[:polymorphic]
+				klass = if options[:polymorphic]
 					read_attribute("#{name}_type".to_sym).classify
 				else
 					name.to_s.classify
-				end.constantize.select(*args).scoped(scope).finded(self, name)
+				end
+				if args.empty?
+					klass.select('*').scoped(scope).finded(self, name)
+				else
+					klass.constantize.select(*args).scoped(scope).finded(self, name)
+				end
 			end
 		end
 
